5d5d47d2e4333cd4fd5cf721220c12224ce0fe28
toandh <toandh.thuylinh@gmail.com>
2024-10-11 07:41:13 +0700

    update config multisite

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -125,6 +125,8 @@ define('DOMAIN_CURRENT_SITE', 'localhost:8000');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
+define('FS_METHOD', 'direct');
+//define('FS_METHOD', 'direct');
 
 /* Add any custom values between this line and the "stop editing" line. */
 
